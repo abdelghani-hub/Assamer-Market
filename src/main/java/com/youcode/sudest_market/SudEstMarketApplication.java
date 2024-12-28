e215869b347ed4a88042716eccbda8ff743e9204
abdelghani <aaittamghart9@gmail.com>
2024-12-28 14:53:36 +0100

    initialize spring boot project

---
+++ src/main/java/com/youcode/sudest_market/SudEstMarketApplication.java
@@ -0,0 +1,13 @@
+package com.youcode.sudest_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudEstMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SudEstMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youcode/sudest_market/SudEstMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youcode.sudest_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudEstMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
