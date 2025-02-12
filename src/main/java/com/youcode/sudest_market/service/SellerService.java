ac603417e6025b8663d974008cb8419728a7c5b0
abdelghani <aaittamghart9@gmail.com>
2025-02-12 11:07:16 +0100

    ref: add accept/reject seller request

--- src/main/java/com/youcode/sudest_market/service/SellerService.java
+++ src/main/java/com/youcode/sudest_market/service/SellerService.java
@@ -1,9 +1,13 @@
 package com.youcode.sudest_market.service;
 
 import com.youcode.sudest_market.domain.SellerRequest;
+import com.youcode.sudest_market.domain.enums.RequestStatus;
+
+import java.util.UUID;
 
 public interface SellerService {
 
     SellerRequest createSellerRequest();
 
+    boolean updateSellerRequest(UUID requestId, RequestStatus status);
 }
